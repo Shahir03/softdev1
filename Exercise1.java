dd9c3a2d2c9647e3bb47f62c9c6091140427cdd5
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2024-04-22 11:30:46 +0100

    Update Exercise1.java

--- Exercise1.java
+++ Exercise1.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
-public class QuizExapmle{
+public class Exercise1{
     public static void main(String[] args){
-        Scanner input= new Scanner(System.in);
+String name= "Jim";
+String job= "Carpenter";
+int age= 47;
+double earnings= 878.23;
+
+System.out.println(name + " works as a " + job);
+System.out.println(" He is " + age + "years old");
+System.out.println("He earns " + " earnings + "after tax");
+
 
 
      input.close();
